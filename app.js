d83c0ad26de72334be22bae2a091e597a26adf99
jacksonz16 <144455915+jacksonz16@users.noreply.github.com>
2023-10-26 00:34:40 -0400

    2

--- app.js
+++ app.js
@@ -1,7 +1,39 @@
-console.log("button ");
-const domselector  = {
-    button : document.getElementById('button'),
-    cardkidnapper: document.querySelectorAll(".card")
-
-
-}
+const DOMselectors = {
+    formA: document.getElementById("form-example"),
+    container: document.getElementById("data"),
+    button: document.getElementById("submit"),
+    value1: document.querySelector(".input1"),
+    value2: document.querySelector(".input2"),
+    value3: document.querySelector(".input3"),
+  };
+  
+  function create() {
+    const v1 = DOMselectors.value1.value;
+    const v2 = DOMselectors.value2.value;
+    const v3 = DOMselectors.value3.value;
+    DOMselectors.container.insertAdjacentHTML(
+      "afterend",
+      `<div id="newbox">
+      <p>${v1}</p>
+      <p>${v2}</p> 
+      <p>${v3}</p>
+      <button class="btn">Erase Me</button>
+    </div>
+      `
+    );
+  }
+  
+  function erase() {
+    let remove = document.querySelectorAll(".btn");
+    remove.forEach((button) => {
+      button.addEventListener("click", (clicks) => {
+        clicks.target.parentElement.remove();
+      });
+    });
+  }
+  DOMselectors.button.addEventListener("click", function (box) {
+    box.preventDefault();
+     create();
+     erase();
+   });
+   console.log(DOMselectors);
